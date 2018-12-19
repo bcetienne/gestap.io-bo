164f7af840a88767c1ac6bc4fdd0b8d7af7da4e8
Etienne <etienne1296pro@gmail.com>
2018-12-19 15:26:38 +0000

    Add Mongoose and routes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+// var MongoClient = require('mongodb').MongoClient;
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* For testing */
+router.get('/test/:user?', function(req, res, next) {
+  // Retrieve the value of :user (eg for /test/max in URI: {user: "max"})
+  console.log(req.params);
+  // Retrieve each values after ? (eg for /test/max?id=3 in URI: {id: "3"})
+  console.log(req.query);
 });
+
+/* GET list of users */
+router.get('/', function (req, res, next) { 
+  res.send('List of users');
+});
+
+/* POST insert user */
+// router.post();
+
+/* PUT update user */
+// router.put();
+
+/* DELETE user */
+// router.delete();
 
 module.exports = router;
