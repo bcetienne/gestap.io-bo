452fff01df2cdd6c52e64459dfc5ffe11c6539ee
Etienne BLANC-COQUAND <etienne.blanc-coquand@kgp-agency.com>
2019-01-10 15:06:29 +0100

    Prepare for records routes

--- routes/records.js
+++ routes/records.js
@@ -1,8 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function (req, res, next) {
+/**
+ * GET all records
+ */
+router.get('/all', function (req, res, next) {
   res.send('List of records');
 });
+
+/**
+ * GET one record
+ */
+router.get('/one', function (req, res, next) {
+  res.send('One record');
+});
+
+/**
+ * ADD one record
+ */
+
+/**
+ * UPDATE one record
+ */
+
+/**
+ * DELETE one record
+ */
 
 module.exports = router;
