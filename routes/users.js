64d02eda9a1b9e12fe873a2a497a00b0cd5f1e4c
Etienne BLANC-COQUAND <etienne.blanc-coquand@kgp-agency.com>
2019-01-14 14:01:48 +0100

    test

--- routes/users.js
+++ routes/users.js
@@ -165,6 +165,7 @@ router.post('/add', function (req, res, next) {
     let userExists = false;
     // Check if the mail is already registered
     User.find({email: data.email}, function (errEmail, responseEmail) {
+      res.send(responseEmail);
       userExists = responseEmail.length !== 0;
     });
 
