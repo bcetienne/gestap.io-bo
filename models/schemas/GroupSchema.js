b95adf079ba7eca464844288236e9491c7cb7fdf
Etienne BLANC-COQUAND <etienne.blanc-coquand@kgp-agency.com>
2019-01-14 20:06:08 +0100

    Update models

--- models/schemas/GroupSchema.js
+++ models/schemas/GroupSchema.js
@@ -1,7 +1,17 @@
-let mongoose = require('mongoose');
-let Schema = mongoose.Schema;
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-new Schema({
-  name: String,
-  users: Array
+const GroupSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: false
+  },
+  users: {
+    type: Array,
+    required: false,
+    unique: false
+  }
 });
+
+const Group = module.exports = mongoose.model('Group', GroupSchema);
--- models/schemas/UserSchema.js
+++ models/schemas/UserSchema.js
@@ -1,12 +1,46 @@
-let mongoose = require('mongoose');
-let Schema = mongoose.Schema;
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-new Schema({
-  firstname: String,
-  lastname: String,
-  email: String,
-  password: String,
-  birthday: String,
-  rfid: Array,
-  admin: Boolean
+const UserSchema = mongoose.Schema({
+  firstname: {
+    type: String,
+    required: true,
+    unique: false
+  },
+  lastname: {
+    type: String,
+    required: true,
+    unique: false
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+    unique: false
+  },
+  birthday: {
+    type: String,
+    required: false,
+    unique: false
+  },
+  rfid: {
+    type: Array,
+    required: false,
+    unique: true
+  },
+  admin: {
+    type: Number,
+    required: true,
+    unique: false
+  }
 });
+
+const User = module.exports = mongoose.model('User', UserSchema);
+
+module.exports.getUserById = function (userId, callback) {
+  User.findOne({_id: userId}, callback);
+};
